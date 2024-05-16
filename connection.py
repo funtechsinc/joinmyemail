9b7e7bc7e0d9e048d5e9e1630140485020498909
iddrismystic <funtechs45@gmail.com>
2024-05-16 02:24:29 +0000

    i

--- connection.py
+++ connection.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.sql_model import SQLBASE
 
-uri = 'postgresql://postgres:admin@localhost:5432/SubscribeToMyEmailList'
+# uri = 'postgresql://postgres:admin@localhost:5432/SubscribeToMyEmailList'
+uri = 'sqlite:///SubscribeToMyEmail.db'
 engine = create_engine(uri, pool_size=1000, max_overflow=2000)
 
 # bind to create all tables
